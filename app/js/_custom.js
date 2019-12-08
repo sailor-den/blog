352ad6343b1dbdb89aedbdd4f89d53072998ab2b
Denis Preobrazhensky <obraz.den@gmail.com>
2019-12-08 21:12:40 +0300

    add css animation

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,68 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    function smokeeffect () { 
+        var modalTrigger = $('.nav-icon'),
+            transitionLayer = $('.cd-transition-layer'),
+            transitionBackground = transitionLayer.children(),
+            modalWindow = $('.modal-menu');
+    
+        var frameProportion = 1.78, //png frame aspect ratio
+            frames = 25, //number of png frames
+            resize = false;
+    
+        //set transitionBackground dimentions
+        setLayerDimensions();
+        $(window).on('resize', function(){
+            if( !resize ) {
+                resize = true;
+                (!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
+            }
+        });
+    
+        //open modal window
+        modalTrigger.on('click', function(event){   
+            event.preventDefault();
+            transitionLayer.addClass('visible opening');
+            var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 600;
+            setTimeout(function(){
+                modalWindow.addClass('visible');
+            }, delay);
+        });
+    
+        //close modal window
+        modalWindow.on('click', '.modal-close', function(event){
+            event.preventDefault();
+            transitionLayer.addClass('closing');
+            modalWindow.removeClass('visible');
+            transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+                transitionLayer.removeClass('closing opening visible');
+                transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+            });
+        });
+    
+        function setLayerDimensions() {
+            var windowWidth = $(window).width(),
+                windowHeight = $(window).height(),
+                layerHeight, layerWidth;
+    
+            if( windowWidth/windowHeight > frameProportion ) {
+                layerWidth = windowWidth;
+                layerHeight = layerWidth/frameProportion;
+            } else {
+                layerHeight = windowHeight*1.2;
+                layerWidth = layerHeight*frameProportion;
+            }
+    
+            transitionBackground.css({
+                'width': layerWidth*frames+'px',
+                'height': layerHeight+'px',
+            });
+    
+            resize = false;
+        }
+    
+    }
+    smokeeffect()
 
 
 });
